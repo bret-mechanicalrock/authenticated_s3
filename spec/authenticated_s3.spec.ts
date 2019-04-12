ff462e0d9b9c28402e19b16e5a5e405a0496aed8
Bret Comstock Waldow <bret.waldow@mechanicalrock.io>
2019-04-12 20:47:51 +0800

    Formatting changes

--- spec/authenticated_s3.spec.ts
+++ spec/authenticated_s3.spec.ts
@@ -1,4 +1,4 @@
-import * as request from "request-promise-native"; 
+import * as request from "request-promise-native";
 (global as any).fetch = require('node-fetch');
 import Amplify from 'aws-amplify';
 
@@ -13,13 +13,13 @@ describe("Authenticated S3", () => {
         Auth: {
             // REQUIRED - Amazon Cognito Region
             region: 'us-east-1',
-            
+
             // OPTIONAL - Amazon Cognito User Pool ID
             userPoolId: 'us-east-1_GFLHTFnPy',
-    
+
             // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
             userPoolWebClientId: '65j6nu2kk7du7por84gvu6qo9u',
-            
+
             // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
             authenticationFlowType: 'USER_SRP_AUTH'
         }
@@ -27,18 +27,18 @@ describe("Authenticated S3", () => {
 
     describe("When authenticated against cognito", () => {
         let jwtToken;
-        beforeAll( async (done) => {
-            try{
+        beforeAll(async (done) => {
+            try {
                 jwtToken = await logIntoCognito()
-            }catch(err){
+            } catch (err) {
                 done.fail(err)
             }
-            
+
             done()
         }, 30 * 1000)
 
         it('should grant access to a secured object', async (done) => {
-            try{
+            try {
                 await request({
                     uri: securedObject,
                     json: true,
@@ -47,14 +47,14 @@ describe("Authenticated S3", () => {
                     }
                 })
                 done()
-            }catch(err) {
+            } catch (err) {
                 console.log(err)
                 done.fail("request should have succeeded")
             }
         })
 
         it('should return forbidden (403) for non-existant object', async (done) => {
-            try{
+            try {
                 await request({
                     uri: nonExistantObject,
                     json: true,
@@ -63,7 +63,7 @@ describe("Authenticated S3", () => {
                     }
                 })
                 done.fail("request should have returned '(403)'")
-            }catch(err) {
+            } catch (err) {
                 expect(err.statusCode).toEqual(403)
                 done()
             }
@@ -71,46 +71,41 @@ describe("Authenticated S3", () => {
     })
 
     describe("When unauthenticated", () => {
-        it('should return unauthorised (401) for a secured object', async (done) => {
 
-            try{
+        it('should return unauthorised (401) for a secured object', async (done) => {
+            try {
                 await request({
                     uri: securedObject,
                     json: true
                 })
                 done.fail("request should have failed")
-            }catch(err) {
+            } catch (err) {
                 expect(err.statusCode).toEqual(401)
                 done()
             }
-            
-
-            
         })
 
         it('should return unauthorised (401) for non-existant protected object', async (done) => {
-
-            try{
+            try {
                 await request({
                     uri: nonExistantObject,
                     json: true
                 })
                 done.fail("request should have failed")
-            }catch(err) {
+            } catch (err) {
                 expect(err.statusCode).toEqual(401)
                 done()
             }
         })
 
         it('should return unauthorised (401) for non-existant protected object at any path', async (done) => {
-
-            try{
+            try {
                 await request({
                     uri: nonExistantObjectRoot,
                     json: true
                 })
                 done.fail("request should have failed")
-            }catch(err) {
+            } catch (err) {
                 expect(err.statusCode).toEqual(401)
                 done()
             }
@@ -118,10 +113,9 @@ describe("Authenticated S3", () => {
     })
 
     async function logIntoCognito(): Promise<string> {
-
         Amplify.configure(cognitoDetails);
         await Amplify.Auth.signIn('bret', 'Temporary_1');
         const session = await Amplify.Auth.currentSession()
-        return session.accessToken.jwtToken  
+        return session.accessToken.jwtToken
     }
 })
